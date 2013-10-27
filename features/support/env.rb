19e0ef93a23261011fc5f5ab9fa79e401984ba03
Aliaksei Palkanau <lifusexitus@gmail.com>
2013-10-27 17:45:35 +0300

    Use configuration block.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
 require 'capybara'
 require 'capybara/cucumber'
 
-Capybara.default_driver = :selenium
-Capybara.app_host = 'http://www.google.com' 
-World(Capybara) 
+Capybara.configure do |config|
+  config.default_driver = :selenium
+  config.app_host   = 'http://www.google.com'
+end
+World(Capybara)
